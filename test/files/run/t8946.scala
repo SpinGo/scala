286538b949fda1c1fdd43f0873d855d86436767f
Tim Harper <timcharper@gmail.com>
2014-11-27 01:38:36 -0700

    fix intermitent failure in scala-reflect thread retention test

--- test/files/run/t8946.scala
+++ test/files/run/t8946.scala
@@ -2,17 +2,29 @@
 object Test {
   import scala.ref.WeakReference
 
-  def forceGc() = {
-    var obj = new Object
-    val ref = new WeakReference(obj)
-    obj = null;
+  def nudgeGc() = {
+    val ref = new WeakReference(new Object)
     while(ref.get.nonEmpty)
       Array.ofDim[Byte](16 * 1024 * 1024)
   }
 
+  def anyCollectable(refs: Seq[WeakReference[_]], triesRemaining: Int = 8): Boolean = {
+    nudgeGc()
+    Thread.sleep(25) // give the JVM some time to finish the cycle
+    val retained = refs flatMap (_.get)
+
+    if (retained.length < refs.length) // we collected one!
+      true
+    else if (triesRemaining > 0)
+      anyCollectable(refs, triesRemaining - 1)
+    else
+      false
+  }
+
   def main(args: Array[String]): Unit = {
-    val threads = for (i <- (1 to 16)) yield {
+    val threads: Seq[WeakReference[Thread]] = for (i <- (1 to 8)) yield {
       val t = new Thread {
+        val data = Array.ofDim[Byte](1 * 1024 * 1024)
         override def run(): Unit = {
           import reflect.runtime.universe._
           typeOf[List[String]] <:< typeOf[Seq[_]]
@@ -22,8 +34,7 @@ object Test {
       t.join()
       WeakReference(t)
     }
-    forceGc()
-    val nonGCdThreads = threads.filter(_.get.nonEmpty).length
-    assert(nonGCdThreads == 0, s"${nonGCdThreads} threads were retained; expected 0.")
+
+    assert(anyCollectable(threads), s"All thread objects are being retained; expected: they should be available for garbage collection.")
   }
 }
